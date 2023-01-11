eb246e097239790623a14fc8e96eda7cae043400
ygcorrea <g.yascorrea@gmail.com>
2023-01-10 21:57:32 -0300

    feat: :tada: Starting sticky notes project with vue 3 + vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main id="app">
+      <div class="section__container">
+        <h3>Sticky Notes</h3>
+        <NoteCard title="my card title" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum tempore ad, nemo rerum molestias eum unde similique voluptate saepe voluptas beatae fuga atque, exercitationem ipsa laudantium sed tempora. Architecto, optio!" buttonText="Deletar"/>
+      </div>
+    </v-main>
+  </v-app>
 </template>
+   
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NoteCard from './components/NoteCard.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    NoteCard
+  },
+  data() {
+    return {
+      notes: [
+        {
+          id: 1, text: 'loremipsum',
+        },
+        {
+          id: 2, text: 'loremipsum',
+        }
+      ]
+    }
+  },
+  methods: {
+    addNote() {
+      this.notes.push({
+        id: 3,
+        text: 'loremipsum',
+      })
+    }
   }
 }
 </script>
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin: 0;
+  background: #009578;
 }
+
+h3 {
+  text-align: center;
+}
+
 </style>
---
+++ src/components/NoteCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="mx-auto" max-width="344">
+    <v-card-title>
+      {{ title }}
+    </v-card-title>
+    <v-card-text>
+      <p>{{ content }} </p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text color="teal accent-4" @click="deleteNote(i)">
+        {{ buttonText }}
+      </v-btn>
+    </v-card-actions>
+
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'NoteCard',
+  props: {
+    title: String,
+    content: String,
+    buttonText: String,
+
+  },
+  data: () => ({
+    reveal: false,
+  }),
+}
+</script>
+<style>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
